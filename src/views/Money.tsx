8d0f56a3324401982fcba11356c23cbdeb4d4742
ddwayne <652507565@qq.com>
2021-05-09 22:46:23 +0800

    改造Note组件

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
 import {CategorySection} from './Money/CategorySection';
-import {NotesSection} from './Money/NotesSection';
+import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
 import {TagsSection} from './Money/TagsSection';
 
@@ -14,21 +14,8 @@ const MyLayout = styled(Layout)`
 function Money() {
     return (
         <MyLayout>
-            <TagsSection>
-                <ol>
-                    <li>衣服</li>
-                    <li>住房</li>
-                    <li>餐饮</li>
-                    <li>交通</li>
-                </ol>
-                <button>新增标签</button>
-            </TagsSection>
-            <NotesSection>
-                <label>
-                    <span>备注</span>
-                    <input type="text" placeholder="在这里添加备注"/>
-                </label>
-            </NotesSection>
+            <TagsSection/>
+            <NoteSection/>
             <CategorySection>
                 <ul>
                     <li className="selected">支出</li>
