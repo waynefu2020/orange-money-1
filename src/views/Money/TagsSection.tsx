7111e4e82a7c894784409a7fe5ecc1a25e337b76
ddwayne <652507565@qq.com>
2021-05-09 22:27:46 +0800

    完成标签的选择和新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,27 +1,51 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, {useState} from 'react';
 
-const TagsSection: React.FC =()=>{
+const TagsSection: React.FC = () => {
+    const [tags, setTags] = useState<string[]>(['衣服', '住房', '餐饮', '交通']);
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    const onAddTag = () => {
+        const tagName = window.prompt('新标签名称是？');
+        if (tagName != null) {
+            setTags([...tags, tagName]);
+        }
+    };
+    const onToggleTag = (tag: string) => {
+        const index = selectedTags.indexOf(tag);
+        if (index >= 0) {
+            setSelectedTags(selectedTags.filter(t => t !== tag));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    };
+    const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
     return (
-        <_TagsSection>
+        <Wrapper>
             <ol>
-                <li>衣服</li>
-                <li>住房</li>
-                <li>餐饮</li>
-                <li>交通</li>
+                {tags.map(tag =>
+                    <li key={tag}
+                        onClick={() => {onToggleTag(tag);}}
+                        className={getClass(tag)}
+                    >{tag}</li>
+                )}
             </ol>
-            <button>新增标签</button>
-        </_TagsSection>
-    )
-}
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    );
+};
 
-const _TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #ffffff;
   padding: 12px 16px;
-  flex-grow: 1; display: flex; flex-direction: column;
-  justify-content: flex-end; align-items: flex-start;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+
   > ol {
     margin: 0 -12px;
+
     > li {
       background: #d9d9d9;
       border-radius: 18px;
@@ -29,6 +53,10 @@ const _TagsSection = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+      &.selected{
+        background: #f60;
+        color: white;
+      }
     }
   }
 
@@ -42,4 +70,4 @@ const _TagsSection = styled.section`
   }
 `;
 
-export {TagsSection}
+export {TagsSection};
