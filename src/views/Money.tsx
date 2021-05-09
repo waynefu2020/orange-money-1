9e977fd0125585b7f5ec22406fa60d3042aa605f
ddwayne <652507565@qq.com>
2021-05-09 09:22:46 +0800

    TagSection样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,59 +3,74 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagsSection = styled.section`
+  background: #ffffff; padding: 12px 16px;
+  >ol{ margin: 0 -12px;
+    > li {
+      background: #d9d9d9; border-radius: 18px;
+      display: inline-block; padding: 3px 18px;
+      font-size: 14px; margin: 8px 12px;
+    }
+  }
+  >button{
+    background: none; border: none;
+    border-bottom: 1px solid #333; padding: 2px 4px;
+    color: #666;margin-top: 8px;
+  }
+`;
 
-`
 const NotesSection = styled.section`
-`
+`;
 
 const CategorySection = styled.section`
-`
+`;
 
 const NumberPadSection = styled.section`
-`
+`;
+
+
 function Money() {
-  return (
-    <Layout>
-        <TagsSection>
-            <ol>
-                <li>衣服</li>
-                <li>住房</li>
-                <li>餐饮</li>
-                <li>交通</li>
-            </ol>
-            <button>新增标签</button>
-        </TagsSection>
-        <NotesSection>
-            <span>备注</span>
-            <input type="text"/>
-        </NotesSection>
-        <CategorySection>
-            <ul>
-                <li>支出</li>
-                <li>收入</li>
-            </ul>
-        </CategorySection>
-        <NumberPadSection>
-            <div>100</div>
-            <div>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>OK</button>
-                <button>0</button>
-                <button>.</button>
-            </div>
-        </NumberPadSection>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <TagsSection>
+                <ol>
+                    <li>衣服</li>
+                    <li>住房</li>
+                    <li>餐饮</li>
+                    <li>交通</li>
+                </ol>
+                <button>新增标签</button>
+            </TagsSection>
+            <NotesSection>
+                <span>备注</span>
+                <input type="text"/>
+            </NotesSection>
+            <CategorySection>
+                <ul>
+                    <li>支出</li>
+                    <li>收入</li>
+                </ul>
+            </CategorySection>
+            <NumberPadSection>
+                <div>100</div>
+                <div>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>删除</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清空</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>OK</button>
+                    <button>0</button>
+                    <button>.</button>
+                </div>
+            </NumberPadSection>
+        </Layout>
+    );
 }
 
 export default Money;
