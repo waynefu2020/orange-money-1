27396d4bcd34e38d05582f35d77607bb33acea6e
DDWayne <652507565@qq.com>
2021-05-29 17:52:19 +0800

    创建单独的Input

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from "../../components/Input";
 
 const Wrapper = styled.section`
   background: #f5f5f5;
@@ -32,22 +33,12 @@ type Props = {
 
 const NoteSection: React.FC<Props> = (props)=>{
     const note = props.value;
-    const refInput = useRef<HTMLInputElement>(null)
-    const onBlur = () =>{
-        if(refInput.current != null){
-            props.onChange(refInput.current.value);
-        }
+    const onChange: ChangeEventHandler<HTMLInputElement> = (e) =>{
+        props.onChange(e.target.value);
     }
     return (
         <Wrapper>
-            <label>
-                <span>备注</span>
-                <input type="text" placeholder="在这里添加备注"
-                       ref={refInput}
-                       defaultValue={note}
-                       onBlur={onBlur}
-                />
-            </label>
+            <Input label="备注" type="text" value={note} onChange={onChange}/>
         </Wrapper>
     )
 }
